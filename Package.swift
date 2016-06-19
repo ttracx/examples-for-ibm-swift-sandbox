51c4897a9e9bdb90e7e985ae60f826c7c3b294ad
Naoki Tsutsui <naokitsutsui@me.com>
2016-06-20 07:24:45 +0800

    delete kitura

--- Package.swift
+++ Package.swift
@@ -6,6 +6,5 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 17)
     ]
 )

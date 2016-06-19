362e94075b8b702338982737bb90a3ebf85a88b7
Naoki Tsutsui <naokitsutsui@me.com>
2016-06-20 06:16:52 +0800

    add HeliumLogger

--- Package.swift
+++ Package.swift
@@ -5,5 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
     ]
 )

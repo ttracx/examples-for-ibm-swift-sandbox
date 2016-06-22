37b8aa4b098fcd15db8c881efd4c13198d719150
Naoki Tsutsui <naokitsutsui@me.com>
2016-06-22 17:40:22 +0800

    comment out kitura setting

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 8),
         .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
+        // .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
         // .Package(url:  "https://github.com/openwhisk/openwhisk-client-swift.git", versions: Version(0,0,0)..<Version(1,0,0)),
     ]
 )
